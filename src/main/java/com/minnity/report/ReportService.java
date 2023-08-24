8eb530eac93963f61ae984234bb59c93a944a37d
Mohammad Alissa <mohammad.alissa95@hotmail.com>
2023-08-24 13:36:54 +0200

    Test done. I solved all of the 3 tasks and also did test them.

--- src/main/java/com/minnity/report/ReportService.java
+++ src/main/java/com/minnity/report/ReportService.java
@@ -1,24 +1,52 @@
 package com.minnity.report;
 
-import org.apache.commons.lang3.NotImplementedException;
-
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 
 public class ReportService {
 
   //task 1: Return number of requests that were made for each company. (e.g. companyId -> requestNumber)
   public Map<Integer, Long> calculateNumberOfRequestsPerCompany(List<RequestLog> requestLogs) {
-    throw new NotImplementedException("TODO implement this method");
+    // throw new NotImplementedException("TODO implement this method");
+
+    Map<Integer, Long> numberOfRequests = new HashMap<>(); /** I created an empty hashmap to store the number of requests. **/
+
+    for (RequestLog reqLog : requestLogs) { /** Iterate through the request logs **/
+      int id = reqLog.getCompanyId(); /** Get the company id **/
+      numberOfRequests.put(id, numberOfRequests.getOrDefault(id, 0L) + 1); /** Increase request count by 1 for every request **/
+    }
+    return numberOfRequests;
   }
 
   //task 2: Count and return requests per company that finished with an error HTTP response code (>=400)
   public Map<Integer, RequestLog> findRequestsWithError(List<RequestLog> requestLogs) {
-    throw new NotImplementedException("TODO implement this method");
+    // throw new NotImplementedException("TODO implement this method");
+
+    Map<Integer, RequestLog> numberOfErrors = new HashMap<>();
+
+    for (RequestLog reqLog : requestLogs) {
+      int id = reqLog.getCompanyId();
+      if (reqLog.getRequestStatus() >= 400) numberOfErrors.put(id, reqLog);
+    }
+    return numberOfErrors;
   }
 
   //task 3: find and print API (requests path) that on average takes the longest time to process the request.
   public String findRequestPathWithLongestDurationTime(List<RequestLog> requestLogs) {
-    throw new NotImplementedException("TODO implement this method");
+    // throw new NotImplementedException("TODO implement this method");
+
+    RequestLog longestRequestPath = null;
+    long longestTime = Long.MIN_VALUE;
+
+    for (RequestLog reqLog : requestLogs) {
+      long duration = reqLog.getRequestDuration();
+      if (duration > longestTime) longestTime = duration;
+      longestRequestPath = reqLog;
+    }
+    System.out.println("Request duration = " + longestRequestPath.getRequestDuration() + " ms, "
+            + "Request path: " + longestRequestPath.getRequestPath());
+    return longestRequestPath.getRequestPath();
   }
 }
--- src/test/java/com/minnity/report/ReportServiceTest.java
+++ src/test/java/com/minnity/report/ReportServiceTest.java
@@ -1,12 +1,74 @@
 package com.minnity.report;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 
 public class ReportServiceTest {
 
-  @Test
-  public void todo() {
-    RequestLog sampleRequest = SampleDataGenerator.aRequestLog();
+  List<RequestLog> requestLogs;
+  ReportService reportService;
+
+  @Before
+  public void setup() {
+    requestLogs = new ArrayList<>();
+    RequestLog sampleRequest1 = RequestLog.RequestLogBuilder.aRequestLog()
+            .withCompanyId(1).withRequestStatus(201).withRequestDuration(200).withRequestPath("api/user").build();
+    RequestLog sampleRequest2 = RequestLog.RequestLogBuilder.aRequestLog()
+            .withCompanyId(1).withRequestStatus(404).withRequestDuration(150).withRequestPath("api/user").build();
+    RequestLog sampleRequest3 = RequestLog.RequestLogBuilder.aRequestLog()
+            .withCompanyId(2).withRequestStatus(201).withRequestDuration(100).withRequestPath("api/admin").build();
+    RequestLog sampleRequest4 = RequestLog.RequestLogBuilder.aRequestLog()
+            .withCompanyId(3).withRequestStatus(500).withRequestDuration(300).withRequestPath("api/person").build();
+    requestLogs.add(sampleRequest1);
+    requestLogs.add(sampleRequest2);
+    requestLogs.add(sampleRequest3);
+    requestLogs.add(sampleRequest4);
   }
-}
+
+  @Test
+  public void calculateNumberOfRequestsPerCompanyTest() {
+    reportService = new ReportService();
+    Map<Integer, Long> test = reportService.calculateNumberOfRequestsPerCompany(requestLogs);
+    long expected1 = 2;
+    long expected2 = 1;
+    assertEquals(expected1, test.get(1).longValue());
+    assertEquals(expected2, test.get(2).longValue());
+    assertEquals(expected2, test.get(3).longValue());
+  }
+
+  @Test
+  public void findRequestsWithErrorTest() {
+    reportService = new ReportService();
+    Map<Integer, RequestLog> test = reportService.findRequestsWithError(requestLogs);
+    for (RequestLog reqLog : requestLogs) {
+      int id = reqLog.getCompanyId();
+      if (reqLog.getRequestStatus() >= 400) {
+        assertTrue(test.containsKey(id));
+        System.out.println("Company id: " + id + " request: " + reqLog.getRequestStatus() + ": has error: " + reqLog);
+      } else System.out.println("Company id: " + id + " request: " + reqLog.getRequestStatus() + ": has no error");
+    }
+  }
+
+  @Test
+  public void findRequestPathWithLongestDurationTimeTest() {
+    RequestLog longestRequestPath = null;
+    long longestTime = Long.MIN_VALUE;
+
+    for (RequestLog reqLog : requestLogs) {
+      long duration = reqLog.getRequestDuration();
+      if (duration > longestTime) longestTime = duration;
+      longestRequestPath = reqLog;
+    }
+    System.out.println("Request duration = " + longestRequestPath.getRequestDuration() + " ms, "
+            + "Request path: " + longestRequestPath.getRequestPath());
+  }
+
+} //
